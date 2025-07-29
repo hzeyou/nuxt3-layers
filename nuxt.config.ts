80614b138af115d4b7e67ce63ded526e49f58a64
hzeyou <50729632+hzeyou@users.noreply.github.com>
2025-07-30 00:19:27 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+
+const currentDir = dirname(fileURLToPath(import.meta.url))
 export default defineNuxtConfig({
   devtools: { enabled: true },
   $meta: {
     name: 'example',
   },
   css: [
-    '@/assets/main.css'
+    join(currentDir, './assets/main.css')
   ]
 })
